fcec18b41643ec4998204f9062e142c5291a1ad7
JuYoung <jfullover@gmail.com>
2022-06-01 13:05:20 +0900

    메소드 작성

--- src/main/java/kr/co/mis/systemmisproject/mushroom/MushroomController.java
+++ src/main/java/kr/co/mis/systemmisproject/mushroom/MushroomController.java
@@ -1,5 +1,6 @@
 package kr.co.mis.systemmisproject.mushroom;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,22 @@ import org.springframework.web.servlet.ModelAndView;
 @RestController
 @RequestMapping("/mushroom")
 public class MushroomController {
+    @Autowired
+    private MushroomService mushroomService;
+
     @GetMapping
     public ModelAndView main() {
         return new ModelAndView("mushroom/main");
+    }
+
+    @GetMapping
+    public ModelAndView viewMushroom(Mushroom mushroom) {
+        ModelAndView mov = new ModelAndView("mushroom/view");
+
+        Mushroom row = mushroomService.selectOne(mushroom);
+
+        mov.addObject("row", row);
+
+        return mov;
     }
 }
--- src/main/java/kr/co/mis/systemmisproject/mushroom/MushroomServiceImple.java
+++ src/main/java/kr/co/mis/systemmisproject/mushroom/MushroomServiceImple.java
@@ -1,7 +1,15 @@
 package kr.co.mis.systemmisproject.mushroom;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MushroomServiceImple implements MushroomService {
+    @Autowired
+    private MushroomMapper mushroomMapper;
+
+    @Override
+    public Mushroom selectOne(Mushroom mushroom) {
+        return mushroomMapper.selectOne(mushroom);
+    }
 }
